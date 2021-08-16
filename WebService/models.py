e22d5045d85d1528823e024e4e45cf76821c6135
DarlynYami05 <dwverling@gmail.com>
2021-08-15 21:29:09 -0500

    WebServices casi listo

--- WebService/models.py
+++ WebService/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    usuario = models.CharField(max_length=25)
+    clave = models.CharField(max_length=18)
+    nombre1 = models.CharField(max_length=20)
+    nombre2 = models.CharField(max_length=20)
+    apellido1 = models.CharField(max_length=20)
+    apellido2 = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField()
+    ruta_foto = models.ImageField(upload_to="perfil", null=True, blank=False)
+    genero = models.CharField(max_length=1)
+    correo = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'usuarios'
+
+
+class Familiares(models.Model):
+    usuario = models.CharField(max_length=25)
+    clave = models.CharField(max_length=18)
+    nombres = models.CharField(max_length=20)
+    apellidos = models.CharField(max_length=20)
+    ruta_foto = models.ImageField(upload_to="perfil", null=True, blank=False)
+    usuario_paciente = models.ForeignKey(Usuarios, on_delete=models.PROTECT, related_name="Familiares")
+    genero = models.CharField(max_length=1)
+    celular = models.CharField(max_length=10)
+    class Meta:
+        db_table = "familiares"
+
+
+class SaludCardiaca(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.PROTECT, related_name="SaludCardiaca")
+    fecha = models.DateTimeField()
+    class Meta:
+        db_table = 'salud_cardiaca'
+
+
+class BPM(models.Model):
+    salud_cardiaca = models.ForeignKey(SaludCardiaca, on_delete=models.PROTECT, related_name="BPM")
+    bpm = models.CharField(max_length=3)
+    hora = models.DateTimeField()
+    class Meta:
+        db_table = 'bpm'
+
+
+class Alertas(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.PROTECT, related_name="Alertas")
+    familiar = models.ForeignKey(Familiares, on_delete=models.PROTECT, related_name="Alertas")
+    fecha_hora = models.DateTimeField()
+    msj_alerta = models.CharField(max_length=400)
+    class Meta:
+        db_table = 'alertas'
+
+
+class SMS(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.PROTECT, related_name="SMS")
+    familiar = models.ForeignKey(Familiares, on_delete=models.PROTECT, related_name="SMS")
+    fecha_hora = models.DateTimeField()
+    msj_sms = models.CharField(max_length=400)
+    class Meta:
+        db_table = 'sms'
--- WebService/views.py
+++ WebService/views.py
@@ -19,3 +19,27 @@ class Registrarse(APIView):
         if request.method == 'POST':
             return Response({'response': 'POST de Registrarse en mantenimiento'})
         return Response({'response': 'Clase Registrarse en mantenimiento'})
+
+"""
+try:
+    json_data = json.loads(request.body.decode('utf-8'))
+    cedula = json_data['cedula']
+    nombres = (json_data['nombres']).split()
+    nombre1 = nombres[0]
+    nombre2 = nombres[1]
+    apellido1 = nombres[2]
+    apellido2 = nombres[3]
+    unCiudadano = ciudadanos.objects.get(cedula=cedula, nombre1__icontains=nombre1, nombre2__icontains=nombre2, apellido1__icontains=apellido1, apellido2__icontains=apellido2)
+    json_consulta = {
+        "nombres": json_data['nombres'].upper(),
+        "provincia": unCiudadano.provincia.provincia,
+        "canton": unCiudadano.canton.canton,
+        "centro_vacunacion": unCiudadano.centroVacunacion.centro_vacunacion,
+        "direccion": unCiudadano.centroVacunacion.direccion,
+        "primera_dosis": unCiudadano.primeraDosis,
+        "segunda_dosis": unCiudadano.segundaDosis
+    }
+    return Response({"consulta": json_consulta})    
+except ciudadanos.DoesNotExist:
+    return Response({"mensaje": "Ups, no se encuentra registrado...."})
+"""
