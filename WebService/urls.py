48026c5c9170dc631f8cb9f596404dd30748bab5
Henry Pérez <74738884+HenryGP14@users.noreply.github.com>
2021-08-19 11:06:14 -0500

    Arreglo de la vista del login

--- WebService/urls.py
+++ WebService/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('inicio-sesion/', views.InicioSesion.as_view()),
-    path('registrar-paciente/', views.RegistrarPaciente.as_view()),
-    path('registrar-familiar/', views.RegistrarFamiliar.as_view()),
+    path("inicio-sesion/", views.InicioSesion.as_view()),
+    path("registrar-paciente/", views.RegistrarPaciente.as_view()),
+    path("registrar-familiar/", views.RegistrarFamiliar.as_view()),
+    path("mget/", views.InicioSesion.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
