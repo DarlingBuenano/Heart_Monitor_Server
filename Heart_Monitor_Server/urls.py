a29b59d36a9a4555043d397bc5bbeb95f2f2209f
Henry Pérez <dwverling@gmail.com>
2021-08-27 01:13:28 -0500

    app server terminada

--- Heart_Monitor_Server/urls.py
+++ Heart_Monitor_Server/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from AppServer.urls import views as vw_appS
 
 urlpatterns = (
     [
         path('admin/', admin.site.urls),
         path('webservice/', include('WebService.urls')),
+        
+        path('', vw_appS.vwIndex),
         path('appserver/', include('AppServer.urls')),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
